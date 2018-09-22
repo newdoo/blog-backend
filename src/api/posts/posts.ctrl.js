b60b1c753cfca04b1c8e26e8999e46d62fd03f1c
DESKTOP-71BE5MQ\doo <doo8866@numixent.com>
2018-09-22 19:24:15 +0900

    1

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -52,6 +52,11 @@ exports.list = async(ctx) => {
     // page가 주어지지 않았다면 1로 간주
     // query는 문자열 형태로 받아 오므로 숫자로 변환
     const page = parseInt(ctx.query.page || 1, 10);
+    const { tag } = ctx.query;
+
+    const query = tag ? {
+        tags: tag // tags 배열에 tag를 가진 포스트 찾기
+    } : {};
 
     // 잘못된 페이지가 주어졌다면 오류
     if(page < 1) {
@@ -60,23 +65,23 @@ exports.list = async(ctx) => {
     }
 
     try {
-        const posts = await Post.find()
+        const posts = await Post.find(query)
             .sort({_id: -1})
             .limit(10)
             .skip((page-1)*10)
             .exec();
         
-        const postCount = await Post.count().exec();
-        // 마지막 페이지 알려주기
-        // ctx.set은 response header를 설정
-        ctx.set('Last-Page', Math.ceil(postCount/10));
-
+        const postCount = await Post.count(query).exec();
+    
         const limitBodyLength = post => ({
             ...post.toJSON(),
-            body: post.body.length < 200 ? post.body: `${post.body.slice(0,200)}...`
+            body: post.body.length < 350 ? post.body: `${post.body.slice(0,350)}...`
         });
 
         ctx.body = posts.map(limitBodyLength);
+        // 마지막 페이지 알려주기
+        // ctx.set은 response header를 설정
+        ctx.set('Last-Page', Math.ceil(postCount/10));
     } catch(e) {
         ctx.throw(e,500);
     }
